6aff5f356743bb71cec0f1a024b27bb0b91c9d93
Parulshuklaa <145009305+Parulshuklaa@users.noreply.github.com>
2026-02-04 09:49:30 +0530

    Update majority-element.py

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,15 +1,15 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = 0
-        candidate = None
+        element = None
 
         for num in nums:
             if count == 0:
-                candidate = num
-            if num == candidate:
+                element = num
+            if num == element:
                 count += 1
             else:
                 count -= 1
 
-        return candidate
+        return element 
  
