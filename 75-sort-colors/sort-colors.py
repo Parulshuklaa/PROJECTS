53914e597a61a32d8c8eb59f42bb60875c693d4a
Parulshuklaa <145009305+Parulshuklaa@users.noreply.github.com>
2026-02-07 15:36:10 +0530

    Time: 3 ms (11.70%) | Memory: 19.3 MB (52.33%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -5,3 +5,17 @@ class Solution:
             for j in range(0, n - i-1):
                   if nums[j] > nums[j + 1]:
                      nums[j], nums[j+1] = nums[j+1], nums[j] 
+
+
+#for v in nums:
+  #if v==0:
+    #count0+=1
+  #elif v==1:
+    #count1+=1
+  #else:
+    #count2+=1
+#count0=count1=count2=0
+#ind>0
+#while (ind < count2):
+    #nums[ind]=1
+    #ind +=1 
