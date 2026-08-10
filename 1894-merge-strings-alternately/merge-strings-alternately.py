eaab01a4bf50e8d0bc5fb61d30bccc21db164f2e
Parulshuklaa <145009305+Parulshuklaa@users.noreply.github.com>
2026-08-10 11:11:45 +0530

    Time: 44 ms (65.00%) | Memory: 19.4 MB (17.85%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        out = []
+        for i , j in zip(word1,word2):
+            out.append(i)
+            out.append(j)
+        
+        out.append(word1[len(word2):])
+        out.append(word2[len(word1):])
+
+        return "".join(out)
+        
+        
